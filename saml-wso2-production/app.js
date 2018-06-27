daad6040e18178bc9e2ad9c81df6f5d8265ad414
jpope33 <jpope33@uncc.edu>
2018-06-27 13:11:17 -0400

    User being stored in Mongo DB, certs being read by file, using SHA256

--- saml-wso2-production/app.js
+++ saml-wso2-production/app.js
@@ -1,31 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-//const mongoose = require('mongoose');
+const session = require('express-session');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-
+const mongoose = require('mongoose');
 const app = express();
 
+// define route(s)
 const samlRoute = require('./routes/saml');
-
-const samlStrategy = require('./config/saml-strategy');
 
 // setup view engine
 app.set('view engine', 'ejs');
 
-/*
 // connect to mongodb
 mongoose.connect('mongodb://admin:admin@ds231720.mlab.com:31720/oauth-test', ()=> {
 	console.log("Connected to MongoDB");
-});*/
+});
 
-// setup cookie
+// setup cookie, required for SLO
 app.use(cookieSession({
 	maxAge:24*3600*1000,
 	keys:['password']
 }));
 
+/* THIS DOESN'T SEEM NECESSARY?
 // initialize passport for cookie use
+app.use(session({
+	secret: 'secret',
+	saveUninitialized: true,
+	resave: true
+}));*/
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -44,10 +48,10 @@ app.use(logger);
 // create home route
 app.get('/', (req,res)=> {
 	// check is user is logged in
-	if (req.user) {
-		console.log("Auth: " + JSON.stringify(req.user));
+	if (req.isAuthenticated()) {
+		console.log("AUTHENTICATED: " + JSON.stringify(req.user));
 	}else {
-		console.log("Not Auth: " + JSON.stringify(req.user));
+		console.log("NOT AUTHENTICATED: " + JSON.stringify(req.user));
 	}
   res.render('home');
 });
--- saml-wso2-production/routes/saml.js
+++ saml-wso2-production/routes/saml.js
@@ -1,5 +1,67 @@
 const router = require('express').Router();
 const passport = require('passport');
+const SamlStrategy = require('passport-saml').Strategy;
+const fs = require('fs');
+const User = require('../model/wso2user');
+
+passport.serializeUser((user,done)=> {
+  done(null, user);
+});
+
+passport.deserializeUser((user,done)=> {
+  done(null, user);
+});
+
+const samlStrategy = new SamlStrategy({
+  // strategy options
+  issuer: 'WSO2app',
+
+  // login endpoints
+  entryPoint: 'https://localhost:9443/samlsso', // consumes SAML req, issues SAML res
+  callbackUrl: 'http://localhost:3001/saml/login/callback', // consumes SAML res
+
+  // logout endpoints
+  logoutUrl: 'https://localhost:9443/samlsso?slo=true',
+  logoutCallbackUrl: 'http://localhost:3001/saml/logout/callback',
+
+  signatureAlgorithm: 'sha256',
+
+  // public cert .pem format, removed --HEADER-- and --FOOTER--
+	cert: fs.readFileSync('/home/jonathan/Desktop/NewKeyStuff/serverCertTwo.pem', 'utf-8'),
+  // private cert .pem format
+  privateCert: fs.readFileSync('/home/jonathan/Desktop/NewKeyStuff/clientKeyDecryptTwo.pem', 'utf-8')
+
+
+}, (profile, done)=> {
+  /*
+  // Create user and serialize entire user object
+  user = {
+    username: profile['http://wso2.org/claims/username'],
+    email: profile.nameID,
+    firstName: profile['http://wso2.org/claims/givenname'],
+    lastName: profile['http://wso2.org/claims/lastname'],
+    nameID: profile.nameID,
+    nameIDFormat:profile.nameIDFormat
+  };
+  console.log("User Object: " + JSON.stringify(user));
+  return done(null, user);
+  */
+
+  // Store users in MongoDB
+  new User({
+    username: profile['http://wso2.org/claims/username'],
+    email: profile.nameID,
+    firstName: profile['http://wso2.org/claims/givenname'],
+    lastName: profile['http://wso2.org/claims/lastname'],
+    nameID: profile.nameID,
+    nameIDFormat:profile.nameIDFormat
+  }).save().then((user)=> {
+    console.log("User Object: " + user);
+    return done(null, user);
+  });
+});
+
+passport.use(samlStrategy);
 
 router.get('/login',
 	passport.authenticate('saml', {
@@ -19,8 +81,15 @@ router.post('/login/callback',
 );
 
 router.get('/logout', (req,res)=> {
-	req.logout();
-	res.redirect('/');
-});
+  samlStrategy.logout(req, (err, request)=> {
+    if (!err) {
+      //local logout
+      req.logout();
+      //IdP logout
+      //console.log(request);
+      res.redirect(request);
+    }
+  });
+})
 
 module.exports = router;
